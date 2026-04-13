20a4324966b13eebc3cfe1e12b88101e43954840
StevenJohnWeaver <stevenjweaver@gmail.com>
2026-04-13 15:29:47 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     turbonomic = {
       source  = "IBM/turbonomic"
-      version = "1.2.0"
+      version = "1.9.0"
     }
     google = {
       source  = "hashicorp/google"
